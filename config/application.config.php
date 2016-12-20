ff9886bf2bb6cab20f0b8ce8206f672788590bba
jxeng <xf.jiang@sohu.com>
2016-12-20 18:54:16 +0800

    修正配置文件

--- config/application.config.php
+++ config/application.config.php
@@ -5,11 +5,6 @@ return [
     'modules' => [
         'Application',
     ],
-    //模块路径
-    'module_paths' => array(
-        './module',
-        './vendor',
-    ),
     //加载的配置文件（autoload目录下的php文件）
     'config' => [
         'database',
